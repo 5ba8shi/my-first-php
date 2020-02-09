f34ca473ff2fa180cca3322e9dafe4a900711ac7
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-09 11:22:08 +0900

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,24 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
 
-    public function __invoke() {
+        public function __invoke() {
 
-        return <<<EOF
+            return <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body {font-size:16pt; color:#999; }
+    h1 { font-size: 30pt; text-align:right; color:#eee;
+        margin:-15px 0px 0px 0px; }
+    </style>
+    </head>
+    <body>
+        <h1>single Action</h1>
+        <p>これは、シングルアクションコントローラのアクションです。</p>
+    </body>
+    </html>
+    EOF;
+        
     }
 }
