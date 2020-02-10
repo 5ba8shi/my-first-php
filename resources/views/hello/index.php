b6174258d2b63050bb8ad7c39bbb7368c80ac785
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-10 09:58:53 +0900

    え

--- resources/views/hello/index.php
+++ resources/views/hello/index.php
@@ -8,8 +8,8 @@
     </style>
   </head>
   <body>
-        <h1>Index</h1>
-        <p><?php echo $msg; ?></p>
-        <p>ID=<?php echo $id; ?></p>
+    <h1>Index</h1>
+    <p><?php echo $msg; ?></p>
+    <p>ID=<?php echo $id; ?></p>
   </body>
 </html>
