7e2942a7983c7c0a3efaaba6033403314c18185c
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-15 07:02:12 +0900

    Update index.blade.php

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,11 +9,19 @@
   </head>
   <body>
       <h1>Blade/Index</h1>
-      <p>&#064;foreachディレクティブの例</p>
-      <ol>
-        @foreach($data as $item)
-        <li>{{$item}}
-        @endforeach
-      </ol>
+      <p>&#064;forディレクティブの例</p>
+      @foreach ($data as $item)
+        @if ($loop->first)
+          <p>※データ一覧</p>
+          <ul>
+        @endif
+            <li>
+              No,{{$loop->iteration}}. {{$item}}
+            </li>
+        @if ($loop->last)
+          </ul>
+          <p>---ここまで</p>
+        @endif
+      @endforeach
   </body>
 </html>
